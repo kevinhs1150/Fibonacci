9884aebbd07ff45c202d4c02a3e909bc0e9dbf92
Kevin <k-kevin@hotmail.com>
2014-04-23 15:09:29 +0800

    Add a JUnit test file

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -34,7 +34,8 @@ public class Fibonacci {
     }
 
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
+//        int N = Integer.parseInt(args[0]);
+    	final int N = 6;
         for (int i = 1; i <= N; i++)
             System.out.println(i + ": " + fib(i));
     }
