56159f5d7117337d981d0084d661d3d22e278392
Kevin <k-kevin@hotmail.com>
2014-04-24 13:31:17 +0800

    Revisions
    Issue: 11383 (Test bug)

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -1,32 +1,5 @@
 package math;
 
-/*************************************************************************
- *  Compilation:  javac Fibonacci.java
- *  Execution:    java Fibonacci N
- *
- *  Computes and prints the first N Fibonacci numbers.
- *
- *  WARNING:  this program is spectacularly inefficient and is meant
- *            to illustrate a performance bug, e.g., set N = 45.
- *
- *
- *   % java Fibonacci 7
- *   1: 1
- *   2: 1
- *   3: 2
- *   4: 3
- *   5: 5
- *   6: 8
- *   7: 13
- *
- *   Remarks
- *   -------
- *    - The 93rd Fibonacci number would overflow a long, but this
- *      will take so long to compute with this function that we
- *      don't bother to check for overflow.
- *
- *************************************************************************/
-
 public class Fibonacci {
     public static long fib(int n) {
         if (n <= 1) return n;
